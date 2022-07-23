f195b59407ae02bfd544cae3e113bc2cbb0cd130
giosali <70548443+giosali@users.noreply.github.com>
2022-07-23 15:29:10 -0500

    Overload subscript operator to access values from Section objects

--- ini.h
+++ ini.h
@@ -12,12 +12,19 @@ class Section {
 public:
     Section();
 
+    std::string& operator[](const std::string&);
+
 private:
     std::unordered_map<std::string, std::string> m_items;
 };
 
 inline Section::Section()
 {
+}
+
+inline std::string& Section::operator[](const std::string& key)
+{
+    return m_items[key];
 }
 
 } // namespace ini
