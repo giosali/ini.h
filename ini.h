86c25d9d0c97070c6e66506c9a6b94d2736bc31d
giosali <70548443+giosali@users.noreply.github.com>
2022-07-23 15:32:59 -0500

    Implement range-based for loops for Section objects

--- ini.h
+++ ini.h
@@ -13,6 +13,10 @@ public:
     Section();
 
     std::string& operator[](const std::string&);
+    std::unordered_map<std::string, std::string>::const_iterator begin() const;
+    std::unordered_map<std::string, std::string>::iterator begin();
+    std::unordered_map<std::string, std::string>::const_iterator end() const;
+    std::unordered_map<std::string, std::string>::iterator end();
 
 private:
     std::unordered_map<std::string, std::string> m_items;
@@ -25,6 +29,26 @@ inline Section::Section()
 inline std::string& Section::operator[](const std::string& key)
 {
     return m_items[key];
+}
+
+inline std::unordered_map<std::string, std::string>::const_iterator Section::begin() const
+{
+    return m_items.begin();
+}
+
+inline std::unordered_map<std::string, std::string>::iterator Section::begin()
+{
+    return m_items.begin();
+}
+
+inline std::unordered_map<std::string, std::string>::const_iterator Section::end() const
+{
+    return m_items.end();
+}
+
+inline std::unordered_map<std::string, std::string>::iterator Section::end()
+{
+    return m_items.end();
 }
 
 } // namespace ini
