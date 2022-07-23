92d8ff529ad5664704dafc387eeacb3095e8b5ed
giosali <70548443+giosali@users.noreply.github.com>
2022-07-23 15:24:18 -0500

    Implement unordered map to hold section keys and values

--- ini.h
+++ ini.h
@@ -1,6 +1,9 @@
 #ifndef INI_H
 #define INI_H
 
+#include <string>
+#include <unordered_map>
+
 #endif
 
 namespace ini {
@@ -8,6 +11,9 @@ namespace ini {
 class Section {
 public:
     Section();
+
+private:
+    std::unordered_map<std::string, std::string> m_items;
 };
 
 inline Section::Section()
