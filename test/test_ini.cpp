748a0022c2365b59d7b6a3cc0132c01a33a63e7b
giosali <70548443+giosali@users.noreply.github.com>
2022-07-25 23:13:04 -0500

    Rearrange unit tests

--- test/test_ini.cpp
+++ test/test_ini.cpp
@@ -7,166 +7,142 @@
 
 namespace load_stdIfstream {
 
-namespace mustNotThrow {
+// Case
+// The stream is an input file stream of an INI file.
+UTEST(load_stdIfstream, mustNotThrow_inputFileStream)
+{
+    std::ifstream stream("example.ini");
+    ini::File file = ini::load(stream);
+    stream.close();
+}
 
-    // Case
-    // The stream is an input file stream of an INI file.
-    UTEST(load_stdIfstream_mustNotThrow, inputFileStream)
-    {
-        std::ifstream stream("example.ini");
-        ini::File file = ini::load(stream);
-        stream.close();
-    }
-
-} // namespace mustNotThrow
-
-namespace mustThrow {
-
-    // Case
-    // The file stream is closed before being processed.
-    UTEST(load_stdIfstream_mustThrow, streamIsClosed)
-    {
-        std::ifstream stream;
-        stream.close();
-        ASSERT_EXCEPTION(ini::load(stream), std::invalid_argument);
-    }
-
-} // namespace mustThrow
+// Case
+// The file stream is closed before being processed.
+UTEST(load_stdIfstream, mustThrow_streamIsClosed)
+{
+    std::ifstream stream;
+    stream.close();
+    ASSERT_EXCEPTION(ini::load(stream), std::invalid_argument);
+}
 
 } // namespace load_stdIfstream
 
 namespace load_constStdString {
 
-namespace mustNotThrow {
+// Case
+// Input consists of a single section.
+UTEST(load_constStdString, mustNotThrow_singleSection)
+{
+    std::string text = "[Section]";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input consists of a single section.
-    UTEST(load_constStdString_mustNotThrow, singleSection)
-    {
-        std::string text = "[Section]";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input consists of a single section and a few key-value pairs.
+UTEST(load_constStdString, mustNotThrow_singleSectionAndKeyValuePairs)
+{
+    std::string text = "[Section]\nkey = value\nhello = world\nFOO=BAR\nbar : foo";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input consists of a single section and a few key-value pairs.
-    UTEST(load_constStdString_mustNotThrow, single_section_and_key_value_pairs)
-    {
-        std::string text = "[Section]\nkey = value\nhello = world\nFOO=BAR\nbar : foo";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input consists of two different sections where both contain identical
+// key-value pairs.
+UTEST(load_constStdString, mustNotThrow_differentSectionsIdenticalKeyValuePairs)
+{
+    std::string text = "[Section]\nkey = value\nhello = world\nFOO=BAR\nbar : foo\n[Section 2]\nkey = value\nhello = world\nFOO=BAR\nbar : foo";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input consists of two different sections where both contain identical
-    // key-value pairs.
-    UTEST(load_constStdString_mustNotThrow, different_sections_identical_key_value_pairs)
-    {
-        std::string text = "[Section]\nkey = value\nhello = world\nFOO=BAR\nbar : foo\n[Section 2]\nkey = value\nhello = world\nFOO=BAR\nbar : foo";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input consists of multiple empty sections.
+UTEST(load_constStdString, mustNotThrow_multipleEmptySections)
+{
+    std::string text = "[Section 1]\n[Section 2]\n[Section 3]";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input consists of multiple empty sections.
-    UTEST(load_constStdString_mustNotThrow, multiple_empty_sections)
-    {
-        std::string text = "[Section 1]\n[Section 2]\n[Section 3]";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input contains whitespace on the left side of each line.
+UTEST(load_constStdString, mustNotThrow_whiteSpaceLeftSide)
+{
+    std::string text = "     [Section]\n   key = value";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input contains whitespace on the left side of each line.
-    UTEST(load_constStdString_mustNotThrow, whiteSpaceLeftSide)
-    {
-        std::string text = "     [Section]\n   key = value";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input contains whitespace on the right side of each line.
+UTEST(load_constStdString, mustNotThrow_whiteSpaceRightSide)
+{
+    std::string text = "[Section]      \nkey = value        ";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input contains whitespace on the right side of each line.
-    UTEST(load_constStdString_mustNotThrow, whiteSpaceRightSide)
-    {
-        std::string text = "[Section]      \nkey = value        ";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input contains lines that are padded with whitespace on both sides.
+UTEST(load_constStdString, mustNotThrow_whiteSpaceBothSides)
+{
+    std::string text = "      [Section]      \n      key = value       ";
+    ini::File file = ini::load(text);
+}
 
-    // Case
-    // Input contains lines that are padded with whitespace on both sides.
-    UTEST(load_constStdString_mustNotThrow, whiteSpaceBothSides)
-    {
-        std::string text = "      [Section]      \n      key = value       ";
-        ini::File file = ini::load(text);
-    }
+// Case
+// Input contains a missing header.
+UTEST(load_constStdString, mustThrow_missingHeader)
+{
+    std::string text = "key = value\nfoo = bar\n[Section]\nkey = value";
+    ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
+}
 
-} // namespace mustNotThrow
+// Case
+// Input contains a section with duplicate keys.
+UTEST(load_constStdString, mustThrow_duplicateKeys)
+{
+    std::string text = "[Section]\nkey = value\nkey = value";
+    ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
+}
 
-namespace mustThrow {
+// Case
+// Input contains an empty section.
+UTEST(load_constStdString, mustThrow_emptySection)
+{
+    std::string text = "[]\nfoo = bar";
+    ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
 
-    // Case
-    // Input contains a missing header.
-    UTEST(load_constStdString_mustThrow, missingHeader)
-    {
-        std::string text = "key = value\nfoo = bar\n[Section]\nkey = value";
-        ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
-    }
+    text = "[Section]\nfoo = bar\n\n[]\nfoo = bar";
+    ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
+}
 
-    // Case
-    // Input contains a section with duplicate keys.
-    UTEST(load_constStdString_mustThrow, duplicate_keys)
-    {
-        std::string text = "[Section]\nkey = value\nkey = value";
-        ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
-    }
+// Case
+// Section must be created if the input isn't terminated by a newline
+// character.
+UTEST(load_constStdString, sectionDoesNotEndWithNewline)
+{
+    std::string text = "[Section]\nfoo = bar\n[Section 2]";
+    ini::File file = ini::load(text);
+    ASSERT_TRUE(file.has_section("Section 2"));
+}
 
-    // Case
-    // Input contains an empty section.
-    UTEST(load_constStdString_mustThrow, emptySection)
-    {
-        std::string text = "[]\nfoo = bar";
-        ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
-
-        text = "[Section]\nfoo = bar\n\n[]\nfoo = bar";
-        ASSERT_EXCEPTION(ini::load(text), std::invalid_argument);
-    }
-
-} // namespace mustThrow
-
-namespace doesNotEndWithNewline {
-
-    // Case
-    // Section must be created if the input isn't terminated by a newline
-    // character.
-    UTEST(load_constStdString_doesNotEndWithNewline, section)
-    {
-        std::string text = "[Section]\nfoo = bar\n[Section 2]";
-        ini::File file = ini::load(text);
-        ASSERT_TRUE(file.has_section("Section 2"));
-    }
-
-    // Case
-    // Key-value pair must be created if the input isn't terminated by
-    // a newline character.
-    UTEST(load_constStdString_doesNotEndWithNewline, key_value)
-    {
-        std::string text = "[Section]\nfoo = bar";
-        ini::File file = ini::load(text);
-        ASSERT_TRUE(file["Section"]["foo"] == "bar");
-    }
-
-} // namespace doesNotEndWithNewline
+// Case
+// Key-value pair must be created if the input isn't terminated by
+// a newline character.
+UTEST(load_constStdString, keyValueDoesNotEndWithNewline)
+{
+    std::string text = "[Section]\nfoo = bar";
+    ini::File file = ini::load(text);
+    ASSERT_TRUE(file["Section"]["foo"] == "bar");
+}
 
 } // namespace load_constStdString
 
 namespace open_constStdFilesystemPath {
 
-namespace mustNotThrow {
-
-    // Case
-    // Opening and reading an INI file.
-    UTEST(open_constStdFilesystemPath_mustNotThrow, filesystemPath)
-    {
-        ini::File file = ini::open("example.ini");
-    }
-
-} // namespace mustNotThrow
+// Case
+// Opening and reading an INI file.
+UTEST(open_constStdFilesystemPath, mustNotThrow_filesystemPath)
+{
+    ini::File file = ini::open("example.ini");
+}
 
 } // namespace open_constStdFilesystemPath
 
